68be5f517278553145c144929fb13d8686b7f0a6
nazmul <nazmul.apu.hasan@gmail.com>
2016-04-01 16:02:13 +0300

    reading a xml

---
+++ parseXML/src/naz/files/ReadXml.java
@@ -0,0 +1,45 @@
+package naz.files;
+
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class ReadXml {
+	public static void main(String[] args) {
+		try {
+			DocumentBuilderFactory factory =
+					DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse("http://bangla.bdnews24.com/?widgetName=rssfeed&widgetId=1151&getXmlFeed=true");
+			 
+			
+			NodeList list = doc.getElementsByTagName("title");
+			System.out.println("There are " + list.getLength()+ " items");
+			
+			for (int i = 0; i < list.getLength(); i++) {
+				Element item = (Element)list.item(i);
+				System.out.println(item.getFirstChild().getNodeValue());
+				
+			}
+			
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
